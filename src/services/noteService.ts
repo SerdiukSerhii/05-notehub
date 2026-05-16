8907cfe0a9f01623d855b32beefa9a27870caef8
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-05-16 15:33:29 +0300

    add qyery

---
+++ src/services/noteService.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import type { Note, NewNoteBody } from '../types/note';
+
+const API_KEY = import.meta.env.VITE_NOTEHUB_TOKEN;
+const BASE_URL = 'https://notehub-public.goit.study/api';
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${API_KEY}`,
+  },
+});
+
+interface NotesResponse {
+  results: Note[];
+  total_pages: number;
+}
+
+export const fetchNotes = async (
+  query: string,
+  page: number,
+): Promise<NotesResponse> => {
+  const res = await api.get<NotesResponse>(`/notes`, {
+    params: {
+      query,
+      page,
+    },
+  });
+
+  return res.data;
+};
+
+export const createNote = async (newNote: NewNoteBody) => {
+  const res = await api.post<Note>('/notes', newNote);
+  return res.data;
+};
+
+export const deleteNote = async (noteId: string) => {
+  const res = await api.delete<Note>(`/notes/${noteId}`);
+  return res.data;
+};
